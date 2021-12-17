6f9c6f1388bf4165cc9c351833eecd4aff10f24a
Zach Nielsen <znielsen@amazon.com>
2021-12-17 11:39:16 -0800

    Functionality is looking good

--- lua/blockformatter/functions.lua
+++ lua/blockformatter/functions.lua
@@ -12,7 +12,7 @@ function M.format_block_normal(num_lines, col)
 end
 
 function M.format_block(start_line_num, end_line_num, col)
-    print("start: " .. start_line_num .. ", end: " .. end_line_num .. ", col: " .. col)
+    -- print("start: " .. start_line_num .. ", end: " .. end_line_num .. ", col: " .. col)
     -- Get the leading number of spaces from the start line
     local line = vim.fn.getline(start_line_num)
     local whitespace = line:match("^%s+")
@@ -20,14 +20,14 @@ function M.format_block(start_line_num, end_line_num, col)
     if whitespace ~= nil then
         leading_space = whitespace:len()
     end
-    print("leading_space: " .. leading_space)
+    -- print("leading_space: " .. leading_space)
 
     if leading_space >= col then
         print("Leading space of ["..leading_space.."] is greater than requested end column ["..col.."]")
         return
     end
     local leading_space_str = string.rep(" ", leading_space)
-    print("leading_space_str: [" .. leading_space_str .. "]")
+    -- print("leading_space_str: [" .. leading_space_str .. "]")
 
     -- Trim newline + leading whitespace
     local holding_str = ""
@@ -47,30 +47,28 @@ function M.format_block(start_line_num, end_line_num, col)
     do
         -- print("holding_str: [" .. holding_str.."]")
 
-        -- local next_word_regex = "^(%s*[%w%p]+)%s.*"
-        local next_word_regex = "^(%s-.-)%s?.*"
+        local next_word_regex = "^(%s*[%g]+)%s.*"
         local leading_whitespace_regex = "^%s+"
         -- Grab words that will fit in column (minimum 1)
         local num_chars = col - leading_space;
         local line = holding_str:gsub(next_word_regex, "%1", 1)
         holding_str = holding_str:sub(line:len() + 1)
         local next_word = holding_str:gsub(next_word_regex, "%1", 1)
-        print("holding_str: [" .. holding_str.."]")
-        print("starting word: [" .. line .."]")
-        print("next word: [" .. next_word.."]")
-        print("num_chars: "..num_chars..", len()s: " .. line:len()..' + '..next_word:len()..' = '..line:len() + next_word:len())
-        while line:len() + next_word:len() < num_chars and holding_str ~= "" and matches ~= 0
+        -- print("holding_str: [" .. holding_str.."]")
+        -- print("starting word: [" .. line .."]")
+        -- print("next word: [" .. next_word.."]")
+        -- print("num_chars: "..num_chars..", len()s: " .. line:len()..' + '..next_word:len()..' = '..line:len() + next_word:len())
+        while line:len() + next_word:len() < num_chars and holding_str ~= ""
         do
             line = line .. next_word
             holding_str = holding_str:sub(next_word:len() + 1)
-            next_word, matches = holding_str:gsub(next_word_regex, "%1", 1)
-            print("holding_str: [" .. holding_str.."]")
-            print("line so far: [" .. line .."]")
-            print("next word: [" .. next_word.."]")
-            print("num_chars: "..num_chars..", len()s: " .. line:len()..' + '..next_word:len()..' = '..line:len() + next_word:len())
-
-            -- Consolidate whitespace into single space
+            -- Consolidate any leading whitespace into single space
             holding_str = holding_str:gsub(leading_whitespace_regex, " ")
+            next_word, matches = holding_str:gsub(next_word_regex, "%1", 1)
+            -- print("holding_str: [" .. holding_str.."]")
+            -- print("line so far: [" .. line .."]")
+            -- print("next word: [" .. next_word.."]")
+            -- print("num_chars: "..num_chars..", len()s: " .. line:len()..' + '..next_word:len()..' = '..line:len() + next_word:len())
         end
 
         ret_str = ret_str .. leading_space_str .. line .. "\n"
